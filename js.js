8b7df97c8504a7570ef92c06129cc3c9e6d1f04b
Maxim Eliseev <35292056+eliseevma@users.noreply.github.com>
2019-04-17 10:50:07 +0300

    Практика

--- js.js
+++ js.js
@@ -2,19 +2,38 @@
 
 let money = +prompt("Ваш бюджет на месяц?","");
 let time = prompt('Введите дату в формате YYYY - MM - DD',"");
-let expanse = prompt("Введите обязательную статью расходов в этом месяце", "");
-let cost =  +prompt("Во сколько обойдется?","")
+
+
 let appData ={
     budget: money,
     timeData: time,
-    expenses:{
-        expanse: cost
-    },
+    expenses:{},
     income:[],
     savings: false
 };
 
-alert(`Ваш бюджет на 1 день: ${(money-cost)/30} рублей`);
+for (let i = 0; i < 2; i++) {
+    let expanse = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let cost = +prompt("Во сколько обойдется?", "");
+    if (typeof (expanse) === "string" && typeof (expanse) != null && typeof (cost) != null 
+    && expanse != "" && cost != "" && expanse.length < 50) {
+        appData.expenses[expanse] = cost;
+    }
+    console.log('Не верно указанны данные');
+}
+
+appData.budgetPerDay = appData.budget / 30;
+console.log(`Ваш бюджет на 1 день: ${appData.budgetPerDay} рублей`);
+
+if (appData.budgetPerDay < 100) {
+    console.log('Минимальный уровень достатка');    
+} else if (appData.budgetPerDay > 100 && appData.budgetPerDay < 2000){
+    console.log('Средний уровень достатка')
+} else if (appData.budgetPerDay > 2000){
+    console.log('Большой уровень достатка');
+} else{
+    console.log('Произошла ошибка')
+}
 
 /*
 Вопросы к этому заданию
